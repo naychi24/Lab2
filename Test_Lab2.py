01fab9f0f643e9ccb6994e31e26c89a190bceb67
Naychi <naychi.24@ichat.sp.edu.sg>
2025-05-26 22:51:49 +0800

    Exercise3 Complete

--- Test_Lab2.py
+++ Test_Lab2.py
@@ -1,8 +1,24 @@
 import lab2 as test
 
-def find_min_max():
-    min_max = [1,2,3,4,5,6]
-    min = 1
-    max = 6
-    min(min_max) == 1
-    assert test. == 0
+print("Test_Lab2")
+
+def test_find_min_max():
+    inp_list = [10, 12.5, 20.3, 15.8, 18.2]
+
+    result = test.find_min_max(inp_list)
+
+    assert (result == 10, 20.3)
+
+def test_calc_average():
+    inp_list = [10, 12.5, 20.3, 15.8, 18.2]
+
+    result = test.calc_average(inp_list)
+
+    assert (result == 15.36)
+
+def test_calc_median_temperature():
+    inp_list = [10, 12.5, 20.3, 15.8, 18.2]
+
+    result = test.calc_median_temperature(inp_list)
+
+    assert (result == 15.8 )
